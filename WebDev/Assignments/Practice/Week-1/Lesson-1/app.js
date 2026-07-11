e1e955877fe475746cd4ced6174b0f0a1026a560
Shivam Verma <verma.shivam.cs@gmail.com>
2026-07-11 21:40:42 +0530

    JS practice-1

---
+++ WebDev/Assignments/Practice/Week-1/Lesson-1/app.js
@@ -0,0 +1,11 @@
+console.log("Program started");
+
+let lang = "Javascript";
+
+const call = () => {
+  console.log(`Welcome to ${lang}`);
+};
+
+call();
+
+console.log("Program Ended");
---
+++ WebDev/Assignments/Practice/practice1.js
@@ -0,0 +1,48 @@
+// const prompt = require("prompt-sync")();
+
+// // let favColor = prompt("Enter a ");
+// // const heightinCm = prompt("Enter height in Cms");
+// // var PizzaLike = prompt("Pizza like or Not");
+// // console.log("Fav color is " + favColor);
+// // console.log("Height " + heightinCm);
+// // console.log("Pizza Decision " + PizzaLike);
+
+// /* let user = prompt("Enter username"); //Assignment 1 from Complex types 
+// console.log(`Hello , ${user}`); */
+
+// /* let name = prompt("Enter your name :"); // Assignment 2 from Complex types
+// let age = prompt("Enter your age :");
+// let gender = prompt("Enter your gender :");
+// if (gender == "Male") gender = "Mr";
+// else if (gender == "Female") gender = "Mrs";
+// console.log(`Hi,${gender} ${name},your age is ${age}`); */
+
+// // Write a function that takes an array of numbers as input, and returns a new array with only even values. Read about filter in JS
+// /* let arr = [];
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let user = prompt(`Enter ${i + 1} :`);
+//   arr.push(user);
+// }
+// const result = arr.filter((num) => num % 2 == 0);
+// console.log(result); */
+
+// /* Assignment
+// Write a function that takes an array of users as inputs and returns only the users who are more than 18 years old */
+// const adult = () => {
+//   let arrOfUsers = [];
+//   let n = 3;
+//   for (let i = 0; i < n; i++) {
+//     //declare an object first
+//     let user = {};
+//     user.name = prompt(`Enter name of user ${i + 1} :`);
+//     user.age = prompt(`Enter age of user ${i + 1} :`);
+//     arrOfUsers.push(user);
+//   }
+//   console.log(arrOfUsers);
+//   const oldEnough = arrOfUsers.filter((num) => num.age >= 18);
+//   return oldEnough;
+// };
+
+// const result = adult();
+// console.log(result);
